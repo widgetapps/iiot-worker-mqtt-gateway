0998c2195d57bf543ee61034863de26af2ab892e
Darryl Patterson <widgetapps@gmail.com>
2018-12-19 16:42:13 -0500

    Updated config.

--- env/production.js
+++ env/production.js
@@ -4,7 +4,7 @@ module.exports = {
     db: 'mongodb://10.240.162.13,10.240.253.155/one-platform',
     dbOptions: { useNewUrlParser: true },
     mqttoptions: {
-        clientId: 'worker_hydrant',
+        clientId: 'worker_gateway',
         username: 'worker',
         password: process.env.MQTT_PASSWORD || ''
     }
