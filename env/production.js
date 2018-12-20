0422c7093e42bd34558ae11f4fc0f02d17fdc8f8
Darryl Patterson <widgetapps@gmail.com>
2018-12-20 01:44:29 -0500

    Testing...

--- env/production.js
+++ env/production.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     db: 'mongodb://10.240.162.13,10.240.253.155/one-platform?replicaSet=rs0',
-    dbOptions: { useMongoClient: true },
+    dbOptions: { useNewUrlParser: true },
     mqttoptions: {
         clientId: 'worker_gateway',
         username: 'worker',
