223beccc968c88941dcf3c3ab2244e1373732cc8
Darryl Patterson <widgetapps@gmail.com>
2018-12-20 01:42:09 -0500

    Testing...

--- env/production.js
+++ env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    db: 'mongodb://10.240.162.13,10.240.253.155/one-platform',
+    db: 'mongodb://10.240.162.13,10.240.253.155/one-platform?replicaSet=rs0',
     dbOptions: { useMongoClient: true },
     mqttoptions: {
         clientId: 'worker_gateway',
